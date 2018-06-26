da8655dd30c62dc2fd0e7dcb555d1743ec56cc2c
wonseok <wonseok0403@naver.com>
2018-06-26 18:45:21 +0900

    start is fixed

--- start.sh
+++ start.sh
@@ -3,8 +3,9 @@ function getHostAddr() {
 	Addr="$(lwp-request -o text checkip.dyndns.org | awk '{ print $NF }')"
 	echo "$Addr"
 }
+apt-get install libwww-perl -y
 echo "Your address is "
-Addr="$(lwp-request -o text checkip.dyndns.org | awk '{ print $NF }')"
+Addr="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')"
 echo $Addr
 apt-get install docker.io -y
 
